1c94a948dfafa77d9d46e69734ec10f682ec47a1
ravioday <joshiravi7@gmail.com>
2018-01-22 20:23:08 -0500

    Add a way to print arc segments

--- circular-view/CircularView.swift
+++ circular-view/CircularView.swift
@@ -30,47 +30,46 @@ class CircularView: UIView {
         context?.addPath(path)
         
         
-        context?.setLineWidth(1.0)
+        context?.setLineWidth(40.0)
         context?.strokePath()
     }
     
     private func createCircularPathIn(_ rect: CGRect, context: CGContext) -> CGMutablePath {
         let path = CGMutablePath()
         let center = CGPoint(x: rect.size.width / 2, y: rect.size.height / 2)
-        let angles = createPoints()
+        let angles = createArcsOnCircle()
         
-        let angle = angles[0]
-        path.addArc(center: center, radius: 50, startAngle: angle.startAngle, endAngle: angle.endAngle, clockwise: false)
-    
-        let angle1 = angles[1]
+        let radius = CGFloat(100.0)
         
-        
-        let angleOffset = 0.3
-        
-        let x = 50 * cos(angle1.startAngle) + center.x
-        let y = 50 * sin(angle1.startAngle) + center.y
-        
-        // This is now center, but we need a point that corresponds to the start angle
-        path.move(to: CGPoint(x: x, y: y))
-        
-//        path.move(to: center)
-        
-        path.addArc(center: center, radius: 50, startAngle: angle1.startAngle, endAngle: angle1.endAngle, clockwise: false)
+        for (index, arc) in angles.enumerated() {
+            if index > 0 {
+                let x = radius * cos(arc.startAngle) + center.x
+                let y = radius * sin(arc.startAngle) + center.y
+                path.move(to: CGPoint(x: x, y: y))
+            }
+            
+            path.addArc(center: center, radius: radius, startAngle: arc.startAngle, endAngle: arc.endAngle, clockwise: false)
+        }
         return path
     }
     
-    private func createPoints() -> [Arc] {
+    private func createArcsOnCircle() -> [Arc] {
         let totalParts:CGFloat = 10
+        let angularSpacingBetweenArcs  = CGFloat(0.3)
         let totalCircleLengthInRadians = CGFloat(2 * Double.pi)
         
-        let lengthOfEachSubArc = CGFloat(totalCircleLengthInRadians / totalParts)
+        let lengthOfEachSubArc = CGFloat((totalCircleLengthInRadians - 0.3 * totalParts) / totalParts)
+        var nextAngle: CGFloat = 0.0
+        var arcs:[Arc] = []
         
-        let nextAngle: CGFloat = 0.0
-        let endAngle = lengthOfEachSubArc
+        for _ in stride(from: 0, to: totalParts, by: 1) {
+            let start = nextAngle
+            let end = start + lengthOfEachSubArc
+            let arc = Arc(startAngle: start, endAngle: end)
+            arcs.append(arc)
+            nextAngle = end + angularSpacingBetweenArcs
+        }
         
-        let arc = Arc(startAngle:nextAngle, endAngle: endAngle)
-        let arc1 = Arc(startAngle: endAngle + 0.3, endAngle:endAngle + 0.3 + endAngle)
-        
-        return [arc, arc1]
+        return arcs
     }
 }
